037bf5989a20ef1384c91596c06986ce335853c9
TJ Egan <twegan@us.ibm.com>
2016-06-01 14:41:34 +0200

    finish chapter 5

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,58 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 /* 
+ App
+*/
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+})
+
+/* 
+ Header
+*/
+var Header = React.createClass({
+  render: function() {
+    return (
+      <p>Header</p>
+    )
+  }
+})
+
+/* 
+ Order
+*/
+var Order = React.createClass({
+  render: function() {
+    return (
+      <p>Order</p>
+    )
+  }
+})
+
+/* 
+ Inventory
+*/
+var Inventory = React.createClass({
+  render: function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+})
+
+/* 
   StorePicker
 */
-
 var StorePicker = React.createClass({
   render: function() {
     
@@ -21,4 +70,5 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.getElementById('main'));
+// ReactDOM.render(<StorePicker/>, document.getElementById('main'));
+ReactDOM.render(<App/>, document.getElementById('main'));
