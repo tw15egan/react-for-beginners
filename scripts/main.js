ae14c7c3c0450707adad632f28df46a160a866ee
TJ Egan <twegan@us.ibm.com>
2016-06-01 22:58:20 +0200

    finish chapter 11

--- scripts/main.js
+++ scripts/main.js
@@ -28,6 +28,11 @@ var App = React.createClass({
     // set the state
     this.setState({fishes: this.state.fishes});
   },
+  loadSamples: function() {
+    this.setState({
+      fishes : require('./sample-fishes.js')
+    });
+  },
   render: function() {
     return (
       <div className="catch-of-the-day">
@@ -35,7 +40,7 @@ var App = React.createClass({
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
@@ -117,6 +122,7 @@ var Inventory = React.createClass({
       <div>
         <h2>Inventory</h2>
         <AddFishForm {...this.props} />
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
   }
